fabdf61dab9bc8d1369f663dde42f9840bec8ffd
naveen rajan <rajanice1996@gmail.com>
2021-06-25 09:57:46 +0530

    Cleaned the code, does the same work

--- hindu/hindu/spiders/HinduSpider.py
+++ hindu/hindu/spiders/HinduSpider.py
@@ -5,28 +5,29 @@ from ..items import HinduItem
 class HinduSpider(scrapy.Spider):
     name = 'hindu'
     page_number = 2
-    start_urls = [
-        'https://www.thehindu.com/news/national/'
-    ]
+    start_urls = ['https://www.thehindu.com/news/national/']
 
     def parse(self, response):
-
         items = HinduItem()
-
         story_cards = response.css('.Other-StoryCard')
 
+        # Extracting the link from every story card
         for card in story_cards:
-
             page_link = card.xpath('a/@href').extract()
-
             items['page_link'] = page_link
-
             yield items
-        next_page = 'https://www.thehindu.com/news/national/?page=' + str(HinduSpider.page_number) + '/'
 
-        if HinduSpider.page_number < 20:
-            print("****************************************************")
-            print("page number:", HinduSpider.page_number)
-            print("****************************************************")
+        # Making the link for the next page using the current
+        # link and by keeping a count of the page number
+        curr_page = 'https://www.thehindu.com/news/national/?page='
+        next_page = curr_page + str(HinduSpider.page_number) + '/'
+
+        # trying to stop the spider when it reaches 20 pages
+        if HinduSpider.page_number < 10:
             HinduSpider.page_number += 1
-            yield response.follow(next_page, callback=self.parse, dont_filter=True)
+
+            yield response.follow(
+                next_page,
+                callback=self.parse,
+                dont_filter=True
+            )
