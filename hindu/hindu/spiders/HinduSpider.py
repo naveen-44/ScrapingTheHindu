5f350c012b75492c76259d7fe54e662c892d2920
naveen rajan <rajanice1996@gmail.com>
2021-06-25 10:00:08 +0530

    Cleaned the code, does the same work

--- hindu/hindu/spiders/HinduSpider.py
+++ hindu/hindu/spiders/HinduSpider.py
@@ -17,12 +17,9 @@ class HinduSpider(scrapy.Spider):
             items['page_link'] = page_link
             yield items
 
-        # Making the link for the next page using the current
-        # link and by keeping a count of the page number
         curr_page = 'https://www.thehindu.com/news/national/?page='
         next_page = curr_page + str(HinduSpider.page_number) + '/'
 
-        # trying to stop the spider when it reaches 20 pages
         if HinduSpider.page_number < 20:
             HinduSpider.page_number += 1
 
